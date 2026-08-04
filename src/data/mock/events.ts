dc7c25d178776c56ad4a1535d47426bd819e0811
千葉大雅 <taigachiba12@gmail.com>
2026-08-04 21:38:55 +0900

    きやれのイベントを追加

--- src/data/mock/events.ts
+++ src/data/mock/events.ts
@@ -430,4 +430,12 @@ export const mockEvents: CampusEvent[] = [
     ["service"],
     [slot("09:00", "15:00")],
   ),
+  event(
+    "service-map-guide",
+    "オープンキャンパス地図アプリ解説",
+    "学生ホールきやれにて、地図アプリの使い方を解説します。",
+    "sh_room_kiyare",
+    ["service"],
+    [slot("09:00", "15:00")],
+  ),
 ];
